b70be7d08f0310d351a39a23519d2ab9bd2b8819
Jake Campa and Sarah Leahy <jake@>
2017-01-23 11:40:52 -0800

    allow multiple landmark inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,45 @@
-function Destination (place, landmarks, date) {
-  this.place = location;
+function Destination (place, date) {
+  this.place = place;
   this.landmarks = [];
   this.date = date;
+}
+Destination.prototype.addLandmark = function(newLandmark){
+  this.landmarks.push(newLandmark)
+}
+Destination.prototype.alertLandmark = function(){
+  alert(this.landmarks);
 }
 
 
 $(document).ready(function() {
+  $("#add-landmark-button").click(function() {
+    $('#input-form').append('<div class="form-group row">' +
+      '<div class="new-landmark">' +
+            '<label for="landmarks" class="col-2 col-form-label">Landmarks</label>' +
+          '<div class="col-10">'+
+              '<input class="landmarks" type="text" value="landmarks">'+
+        '</div>'+
+          '</div>'+
+          '</div>');
+  });
+
   $("#submit-button").click(function() {
    var inputPlace = $("input#location").val();
-   var inputLandmarks = $("input#landmarks").val();
    var inputDate = $("input#date").val();
-   var input = new Destination(inputPlace,inputLandmarks, inputDate);
-
+   var input = new Destination(inputPlace, inputDate);
+   $(".new-landmark").each(function(){
+    var inputLandmarks2 = $(this).find("input.landmarks").val();
+    input.landmarks.push(inputLandmarks2);
+   });
+  // input.addLandmark(inputLandmarks);
+   //input.alertLandmark();
+   var arrayToString = input.landmarks.join(", ");
    $("#output-stream").append('<div class="panel panel-default">' +
     ' <div class="panel-heading">' + inputPlace +
      '</div>' +
      '<div class="panel-body">' +
        '<ul>' +
-        ' <li>Landmark:</li>' + inputLandmarks +
+        ' <li>Landmark:</li>' + arrayToString +
          '<li>Date: </li>' + inputDate +
        '</ul> '  +
      '</div>' +
